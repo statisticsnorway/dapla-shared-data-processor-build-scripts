d225279a62a3690a84857d32da5493c7182d88ba
skykanin <3789764+skykanin@users.noreply.github.com>
2025-01-20 16:30:09 +0100

    fixup! fixup! fixup! fixup! fixup

--- schema-validation/src/validate_config.py
+++ schema-validation/src/validate_config.py
@@ -3,7 +3,17 @@ import os
 import sys
 import yaml
 
-def validate_config(directory_path: str) -> None:
+def validate_config(environment: str, folder: str, directory_path: str) -> None:
+    """
+    Validate if the config.yaml under the @directory_path is valid.
+    Only uses @environment and @folder arguments to print more informative logs.
+
+    Parameters:
+        @environment: The dapla team environment the source belongs to i.e. 'uh-varer-prod' or 'play-obr-test'
+        @folder: The statistics product folder which contains the configuration file i.e. 'ledstill' or 'sykefra'
+        @directory_path: The absolute directory path inside the GHA runner which contains the config.yaml file
+
+    """
     if not os.path.exists(directory_path):
         print(f'The given directory path "{directory_path}" does not exist')
         sys.exit(1)
@@ -21,14 +31,16 @@ def validate_config(directory_path: str) -> None:
     with open('./config_schema.yaml') as stream:
         config_schema: object = yaml.safe_load(stream.read())
 
+    contextual_path = os.path.join(environment, folder, 'config.yaml')
+
     try:
         validate(config_data, schema=config_schema)
-        print(f'The configuration file "{config_data_path}" is valid!')
+        print(f'The configuration file "{contextual_path}" is valid!')
         sys.exit(0)
     except ValidationError as e:
-        print(f'The configuration file "{config_data_path}" is invalid:\n\n{e}')
+        print(f'The configuration file "{contextual_path}" is invalid:\n\n{e}')
         sys.exit(1)
 
 if __name__ == '__main__':
-  [directory_path, *rest] = sys.argv[1:]
-  validate_config(directory_path)
+  [environment, folder, directory_path, *rest] = sys.argv[1:]
+  validate_config(environment, folder, directory_path)
