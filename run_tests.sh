f288046fc0796e16cd4f7b2cb3de752e947092bd
skykanin <3789764+skykanin@users.noreply.github.com>
2025-01-20 13:49:53 +0100

    test pip installing schema validation deps

---
+++ run_tests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This script verifies user supplied scripts for a source using pyflakes and pytest.
+
+# Exit on error and log all commands
+set -ex
+
+# Source packages from base image build
+source /opt/pysetup/.venv/bin/activate
+
+# Install test requirements
+# cd /workspace/dapla-shared-data-processor-build-scripts/scripts
+python -m pip install -r requirements.txt
+# Run pytests
+# pytest
+# echo "## No errors found by pytest"
+# # Check code with pyflakes
+# cd /workspace/automation/source-data/$ENV_NAME/$FOLDER_NAME
+# echo "## Checking code with pyflakes: ${FOLDER_NAME} in environment:${ENV_NAME}"
+# pyflakes process_source_data.py
+# echo "## No errors found by pyflakes"
