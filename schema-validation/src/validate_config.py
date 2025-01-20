8b8b9285d83b7d0510cbf424c8238e89c41d76bc
skykanin <3789764+skykanin@users.noreply.github.com>
2025-01-20 16:12:22 +0100

    fixup! fixup! fixup! fixup

--- schema-validation/src/validate_config.py
+++ schema-validation/src/validate_config.py
@@ -5,29 +5,29 @@ import yaml
 
 def validate_config(directory_path: str) -> None:
     if not os.path.exists(directory_path):
-        print(f'The given filepath {directory_path} does not exist')
+        print(f'The given directory path "{directory_path}" does not exist')
         sys.exit(1)
+
+    # We only expect to find a config.yaml file
+    config_data_path: str = os.path.join(directory_path, 'config.yaml')
+
+    if not os.path.exists(config_data_path):
+        print(f'Could not find the "config.yaml" file at "{directory_path}"')
+        sys.exit(1)
+
+    with open(config_data_path) as stream:
+        config_data: object = yaml.safe_load(stream.read())
 
     with open('./config_schema.yaml') as stream:
         config_schema: object = yaml.safe_load(stream.read())
 
-    # All files inside the product folder 'source' directory
-    filepaths: os.Path = [os.path.join(directory_path, f) for f in os.listdir(directory_path)]
-
-    for path in filepaths:
-        if not os.path.isfile(path):
-            print(f'Illegal directory structure found at "{path}", only files are allowed under a product source folder.')
-            sys.exit(0)
-        with open(path) as stream:
-            data = yaml.safe_load(stream.read())
-
-        try:
-            validate(data, schema=config_schema)
-            print(f'The configuration file "{path}" is valid!')
-            sys.exit(0)
-        except ValidationError as e:
-            print(f'The configuration file "{path}" is invalid:\n\n{e}')
-            sys.exit(1)
+    try:
+        validate(config_data, schema=config_schema)
+        print(f'The configuration file "{config_data_path}" is valid!')
+        sys.exit(0)
+    except ValidationError as e:
+        print(f'The configuration file "{config_data_path}" is invalid:\n\n{e}')
+        sys.exit(1)
 
 if __name__ == '__main__':
   [directory_path, *rest] = sys.argv[1:]
