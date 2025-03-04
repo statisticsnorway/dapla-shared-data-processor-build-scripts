3a685df5bd193eb41dde84c7655f9cc501cce719
Nicholas <3789764+skykanin@users.noreply.github.com>
2025-03-04 08:31:31 +0100

    fixup: code spelling (#4)

--- schema-validation/src/generateCode.scala
+++ schema-validation/src/generateCode.scala
@@ -91,7 +91,7 @@ def genPseudoTask(
   val pseudoOp = pseudoOperation match
     case PseudoOperation.Depseudo => "Depseudonymize"
     case PseudoOperation.Pseudo   => "Pseudonymize"
-    case PseudoOperation.Repseudo => "Reseudonymize"
+    case PseudoOperation.Repseudo => "Repseudonymize"
 
   val taskBlocks = tasks.map(genTaskBlock).mkString("\n")
 
