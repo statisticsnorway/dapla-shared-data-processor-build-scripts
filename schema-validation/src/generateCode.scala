edef7ca239b881676ef1ff9a4f95fa6df8e817e7
skykanin <3789764+skykanin@users.noreply.github.com>
2025-03-18 10:50:33 +0100

    add debug auth token debug to codegen

--- schema-validation/src/generateCode.scala
+++ schema-validation/src/generateCode.scala
@@ -68,6 +68,12 @@ def templateCode(
     |import sys
 
     |def main(file_path):
+    |    pseudo_client = PseudoClient(
+    |        pseudo_service_url=os.getenv(os.environ['PSEUDO_SERVICE_URL']),
+    |        auth_token=os.getenv(os.environ.get('PSEUDO_SERVICE_AUTH_TOKEN', '')),
+    |    )
+    |     logging.info("AUTH TOKEN:\n\n%s", pseudo_client._PseudoClient__auth_token())
+    |
     |    try:
     |        df = pl.read_parquet(file_path)
     |    except Exception as e:
